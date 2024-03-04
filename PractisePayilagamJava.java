907e238fc8ab78f10d44b472a42231f409b8747e
priyaselvakumar97 <priyaselvakumar276@gmail.com>
2024-03-04 11:12:22 +0530

    Java

---
+++ PractisePayilagamJava.java
@@ -0,0 +1,14 @@
+package payilagam_Java;
+
+public class PractisePayilagamJava {
+	public static void main (String[] args) {
+		int a =100;
+		int b=600;
+		int c=300;
+		int total=a+b+c;
+	System.out.println(total);
+		
+		
+	}
+
+}
