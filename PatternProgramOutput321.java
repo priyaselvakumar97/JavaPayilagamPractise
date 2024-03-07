7a7001497a3611f3db2fa577007e95a04c95320a
priyaselvakumar97 <priyaselvakumar276@gmail.com>
2024-03-07 11:01:35 +0530

    pattern 111 222 333

---
+++ PatternProgramOutput321.java
@@ -0,0 +1,18 @@
+package payilagam_Java;
+
+public class PatternProgramOutput321 {
+
+	public static void main(String[] args) {
+		int row;
+		int col;
+	      	//output 111 222 333
+		for(row=1;row<=3;row++) {
+			for(col=1;col<=3;col++) {
+				System.out.print(row);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
--- PatternProgramsOutput123.java
+++ PatternProgramsOutput123.java
@@ -1,23 +1,23 @@
 package payilagam_Java;
 
-public class PatternPrograms {
+public class PatternProgramsOutput123 {
 
 	
 	public static void main (String[] args) {
 		int i;
 		int j;
 		
+//		output 123 123 123
 		for( i=1; i<=3;i++) {
 			for(j=1;j<=3;j++) {
 				System.out.print(j);
 		
 		}
-			System.out.println();
-			
-		
-	}
-	
+			System.out.println();				
+	}	
 }
-	}
+}
+
 	
 
+                          
