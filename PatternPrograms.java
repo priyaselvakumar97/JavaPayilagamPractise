8fa152ad7553316efc4cce78816ddeb82d8d6b4a
priyaselvakumar97 <priyaselvakumar276@gmail.com>
2024-03-07 09:52:07 +0530

    Prgramms

---
+++ PatternPrograms.java
@@ -0,0 +1,23 @@
+package payilagam_Java;
+
+public class PatternPrograms {
+
+	
+	public static void main (String[] args) {
+		int i;
+		int j;
+		
+		for( i=1; i<=3;i++) {
+			for(j=1;j<=3;j++) {
+				System.out.print(j);
+		
+		}
+			System.out.println();
+			
+		
+	}
+	
+}
+	}
+	
+
